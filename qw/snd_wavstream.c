309cb6c66e49a0fa5f326f9c81898c3c00d00bf0
sezero <sezero@users.sourceforge.net>
2015-08-13 17:00:00 +0300

    snd_wavstream.c (S_StreamWAVBackgroundTrack): now that S_RawSamples expects host-endian data, byte-swap the data before sending.

--- qw/snd_wavstream.c
+++ qw/snd_wavstream.c
@@ -230,7 +230,7 @@ static void S_CloseWAVBackgroundTrack(void)
 
 void S_StreamWAVBackgroundTrack(void)
 {
-	int		samples, maxSamples;
+	int		i, samples, maxSamples;
 	int		read, maxRead, total;
 	float	scale;
 	byte	musicWavData[MAX_RAW_SAMPLES];
@@ -298,6 +298,12 @@ void S_StreamWAVBackgroundTrack(void)
 
 			total+= read;
 //			Con_Printf("Read: %i, Samples: %i, Total: %i\n", read, samples, total);
+		}
+		if (musicWavInfo.width == 2) {
+			total = samples * musicWavInfo.channels;
+			for (i = 0; i < total; i++) {
+				((short *)musicWavData)[i] = LittleShort( ((short *)musicWavData)[i] );
+			}
 		}
 		S_RawSamples (samples, musicWavInfo.rate, musicWavInfo.width, musicWavInfo.channels, musicWavData, true);
 	}
