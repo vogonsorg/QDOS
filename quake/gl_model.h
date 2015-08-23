22d2a3fd9a7007518f6b7f8dcaed9a3da3759b59
maraakate <emoaddict15@gmail.com>
2015-08-23 02:27:36 -0400

    raised some limits for rubicon 2

--- quake/gl_model.h
+++ quake/gl_model.h
@@ -327,7 +327,7 @@ typedef struct {
 	maliasframedesc_t	frames[1];	// variable sized
 } aliashdr_t;
 
-#define	MAXALIASVERTS	1024
+#define	MAXALIASVERTS	2000
 #define	MAXALIASFRAMES	256
 #define	MAXALIASTRIS	2048
 extern	aliashdr_t	*pheader;
--- quake/quakedef.h
+++ quake/quakedef.h
@@ -103,7 +103,8 @@ void    VID_UnlockBuffer (void);
 #define ON_EPSILON              0.1                     // point on plane side epsilon
 
 #define	MAX_MSGLEN			32000	// max length of a reliable message //johnfitz -- was 8000
-#define MAX_DATAGRAM            8000 // FS: Was breaking IPX //32000   // max length of unreliable message //johnfitz -- was 1024
+/* FS: FIXME THIS BREAKS IPX */
+#define MAX_DATAGRAM            32000 // max length of unreliable message //johnfitz -- was 1024
 
 #define DATAGRAM_MTU		1400	// johnfitz -- actual limit for unreliable messages to nonlocal clients
 
