d0bad6ee7ac5f2ef8a73a8912aaab301b562e5e3
maraakate <emoaddict15@gmail.com>
2015-08-27 00:37:57 -0400

    remove the .obj file during clean of CVG, fix some fuck ups in build/clean.sh.  updated lib_dxe

--- 3rdparty/glide3x.src/clean.sh
+++ 3rdparty/glide3x.src/clean.sh
@@ -2,3 +2,4 @@ make -f Makefile.DJ FX_GLIDE_HW=cvg realclean
 make -f Makefile.DJ FX_GLIDE_HW=h3 realclean
 make -f Makefile.DJ FX_GLIDE_HW=h5 realclean
 make -f Makefile.DJ FX_GLIDE_HW=sst1 realclean
+make -f Makefile.DJ FX_GLIDE_HW=sst96 realclean
