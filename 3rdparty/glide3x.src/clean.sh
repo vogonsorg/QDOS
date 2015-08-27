d3214edd727ac5a22b260f93d20899b81a5d0285
maraakate <emoaddict15@gmail.com>
2015-08-27 00:28:33 -0400

    build and clean scripts.  Use -O1 instead of -O2 because of SLI issues for h5 (has no impact on performance)

---
+++ 3rdparty/glide3x.src/clean.sh
@@ -0,0 +1,4 @@
+make -f Makefile.DJ FX_GLIDE_HW=cvg realclean
+make -f Makefile.DJ FX_GLIDE_HW=h3 realclean
+make -f Makefile.DJ FX_GLIDE_HW=h5 realclean
+make -f Makefile.DJ FX_GLIDE_HW=sst1 realclean
