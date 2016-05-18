250b45c3152694b4653a68439cdc8021ef8eb4e4
maraakate <emoaddict15@gmail.com>
2016-05-18 14:28:13 -0400

    Removed QWMaster -- Merged with the DK 1.3 version and moved publicly to: https://bitbucket.org/maraakate/gsmaster

--- utils/qwmaster/build_release.bat
+++
@@ -1,5 +0,0 @@
-@ECHO OFF
-TASKKILL /F /T /IM MSDEV.EXE 
-TASKKILL /F /T /IM MSDEV.COM
-msdev master.dsw /MAKE "master - Win32 Release" /CLEAN
-msdev master.dsw /MAKE "master - Win32 Release" /BUILD
